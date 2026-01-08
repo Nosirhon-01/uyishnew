695ba8bacd1fbb7756e9bfdc427159074bb1b3ff
EMMA PARISnode <yeezyss13@gmail.com>
2026-01-08 09:14:12 +0500

    fix: kod xatolari tuzatildi

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,7 +2,6 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { createUser, findUserByEmail } from "../services/usrers.js";
 
-/* REGISTER */
 export const register = async (req, res) => {
     const { email, password } = req.body;
 
@@ -36,8 +35,7 @@ export const login = async (req, res) => {
             message: "Password noto‘g‘ri",
         });
     }
-
-    // 🔥 TOKEN SHU YERDA
+]
     const token = jwt.sign(
         { id: user.id, email: user.email },
         process.env.JWT_SECRET
